bb769cb8069d6dc51cb8b62fb80640136866f346
eirikmorken <eirikliemorken@gmail.com>
2020-01-07 17:37:45 +0100

    Created simple TodoList using vue with router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/layout/Header';
 export default {
-  name: 'app',
+  name:"app",
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
